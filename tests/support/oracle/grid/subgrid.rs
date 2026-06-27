9bfccc285ba17a74a957edb49b7faa4cc58c7232
bj-data-eng <larks-embassy.5j@icloud.com>
2026-06-27 01:15:56 -0400

    Derive Eq for oracle track spans

--- tests/support/oracle/grid/subgrid.rs
+++ tests/support/oracle/grid/subgrid.rs
@@ -57,7 +57,7 @@ pub fn subgrid_eligibility(input: SubgridEligibilityInput) -> SubgridEligibility
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct TrackSpan {
     pub start: usize,
     pub end: usize,
